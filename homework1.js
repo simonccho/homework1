a5c38e7ddb805a8a0fb597abf35ecb8539177329
simonccho <simonccho@gmail.com>
2025-02-07 17:19:14 -0600

    update

--- homework1.js
+++ homework1.js
@@ -2,3 +2,16 @@
 const d = new Date ();
 let doutput = d.toLocaleDateString();
 document.getElementById("todaysDate").innerHTML = doutput;
+
+function slide(x)
+{
+    let slideValue = x;
+    if (slideValue == 1)
+    {
+        document.getElementById("sliderValue").innerHTML=x + " day"
+    }
+    else
+    {
+        document.getElementById("sliderValue").innerHTML=x + " days";
+    }
+}
